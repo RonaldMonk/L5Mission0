d82e0de895982d3c7676114aa60c6268950a922d
RonaldMonk <ronaldm@missionreadyhq.com>
2026-05-21 16:57:28 +1200

    Had focus on wrong area

--- src/components/ComponentSection.jsx
+++ src/components/ComponentSection.jsx
@@ -3,8 +3,7 @@ export default function ComponentSection() {
   return (
     <div className="componentBox">
       <div>
-        <img src='git add .
-        \src\assets\Mission0Pic.jpg' />
+        <img src='\src\assets\Mission0Pic.jpg' />
         <p>Text</p>
       </div>
       
