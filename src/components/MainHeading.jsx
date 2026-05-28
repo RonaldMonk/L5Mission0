7854bc568f8f23ccb70d979f04f34170f46bb9b0
RonaldMonk <ronaldm@missionreadyhq.com>
2026-05-28 20:49:31 +1200

    Sorted menu bar and ma icon

--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -1,18 +1,25 @@
+import './MainHeading.css';
 export default function MainHeading() {
   
   return (
-    <div>
-      Marketing Association NZ
+    <div className="left-menu">
+      <div>
+        <img id="ma-image" src="/src/assets/ma.png" height="20px" width="30px" />
+        <span>Marketing Association NZ</span>
+      </div>
+      
       <nav>
-        <ul>
-          menu1
-          menu2
-          menu3
-          login
-        </ul>
+        <div className="desktop">
+          <ul className="menu-list">
+            <li><a href="#about">About us</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#login">Login</a></li>
+          </ul>
+        </div>
+        <div className="mobile">
+          <a href="#hamburgerIcon">☰</a>
+        </div>
       </nav>
-      Lorem ipsum
-      dolor sit amet
     </div>
   )
 }
